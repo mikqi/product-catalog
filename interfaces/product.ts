48d9718cd67ed83edb0364244c2b8d7906dd6159
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-05-28 19:31:25 +0700

    add interface product detail

--- interfaces/product.ts
+++ interfaces/product.ts
@@ -4,7 +4,7 @@ export interface IReview {
 }
 
 export interface IAttribute {
-  optionListingType: string
+  optionListingType?: string
   values: [string]
 }
 
@@ -15,11 +15,28 @@ export interface IPrice {
   strikeThroughPriceDisplay: string
 }
 
+export interface IImage {
+  full: string
+  thumbnail: string
+}
+
+export interface IBrand {
+  logo: string
+  name: string
+  official: boolean
+}
+
+export interface ISpecifications {
+  name: string
+  value: string
+}
+
 export interface IProduct {
   attributes: [IAttribute]
   id: string
   brand: string
   defaultSku: string
+  formattedId: string
   images: [string]
   itemCount: number
   itemSku: string
@@ -28,4 +45,19 @@ export interface IProduct {
   review: IReview
   status: string
   tags: [string]
+}
+
+export interface IProductDetail {
+  attributes: IAttribute
+  brand: IBrand
+  condition: [string]
+  description: string
+  images: [IImage]
+  itemCode: string
+  itemSku: string
+  itemSkus: [string]
+  name: string
+  productItemCode: string
+  review: IReview
+  specifications: ISpecifications
 }

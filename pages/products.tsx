f7b503713a34d5994e17cea111370d04c275cd2c
Muhammad Rivki <Muhammad.Rivki@live.com>
2019-05-28 21:15:35 +0700

    add product detail unstyling and no price detail

--- pages/products.tsx
+++ pages/products.tsx
@@ -1,20 +1,46 @@
+import axios from 'axios'
 import React, { Component } from 'react'
+import { IProductDetail } from '../interfaces/product'
 import Layout from '../layouts/default'
 
 export default class extends Component<any, any> {
-  public static getInitialProps({ query: { id, name } }) {
-    return { postId: id, productName: name }
+  public static async getInitialProps(context: any) {
+    const product = context.query
+    const isServer = context.req !== undefined
+    const formattedId = isServer
+      ? context.req.params.formattedId
+      : product.formattedId
+    const defaultSku = isServer
+      ? context.req.query.selectedItemSku
+      : product.defaultSku
+
+    const URL = isServer
+      ? context.req.protocol + '://' + context.req.get('host')
+      : ''
+    const path = `/api/products/${formattedId}?selectedItemSku=${defaultSku}`
+    const fetchUrl = isServer ? URL + path : path
+
+    try {
+      const { data } = await axios.get(fetchUrl)
+      return { product: data.data, defaultSku }
+    } catch (error) {
+      // tslint:disable-next-line:no-console
+      console.log(error)
+    }
   }
 
   public render() {
+    const product: IProductDetail = this.props.product
     return (
-      <Layout title={this.props.productName}>
+      <Layout title={product.name}>
         <div>
-          <h1>My blog post #{this.props.postId}</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </p>
+          {product.images.map((image, i) => {
+            return <img key={i} src={image.full} style={{ width: '100%' }} />
+          })}
+        </div>
+        <h2>{product.name}</h2>
+        <div>
+          <div dangerouslySetInnerHTML={{ __html: product.description }} />
         </div>
       </Layout>
     )
